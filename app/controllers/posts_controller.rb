baa9d1fdf0dd40855175437962033ab09019edca
Matt.C <linkinpark761102@bwwtaiwan.com>
2016-08-26 11:33:40 +0800

    Add and setup Devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,54 @@
 class PostsController < ApplicationController
 	
+	before_action :set_post , :only => [:show,:edit,:update,:destroy]
+  before_action :authenticate_user!
+
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
 
+	def show
+	end
+
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			 redirect_to post_path(@post)
+		else
+			render new_post_path(@post)
+		end
+	end
+
+	def edit
+  end
+	
+
+	def update
+		if @post.update(post_params)
+			redirect_to posts_path
+		else
+			render edit_post_path(@post)
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+end
+
+private
+
+def post_params
+	params.require(:post).permit(:title , :content)
+end	
+
+def set_post
+	@post = Post.find(params[:id])
 end
